db2ff6c3af75e30266563b70116ba0ad6001ab85
Gustavo S. Anibele <gustavoanibele@gmail.com>
2026-02-25 16:18:56 -0300

    update atividade 01
    enviando atividade desenvolvida em aula

---
+++ Aula 01 - Atividade de cadastro em csv/Atividade1/src/main/java/atv1/Produto.java
@@ -0,0 +1,71 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package atv1;
+
+/**
+ *
+ * @author laboratorio
+ */
+public class Produto {
+
+    private String nome;
+    private String codigo;
+    private String descricao;
+    private double preco;
+    private int quantidade;
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getCodigo() {
+        return codigo;
+    }
+
+    public void setCodigo(String codigo) {
+        this.codigo = codigo;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public double getPreco() {
+        return preco;
+    }
+
+    public void setPreco(double preco) {
+        this.preco = preco;
+    }
+
+    public int getQuantidade() {
+        return quantidade;
+    }
+
+    public void setQuantidade(int quantidade) {
+        this.quantidade = quantidade;
+    }
+
+    public Produto(String nome, String codigo, String descricao, double preco, int quantidade) {
+        super();
+        this.nome = nome;
+        this.codigo = codigo;
+        this.descricao = descricao;
+        this.preco = preco;
+        this.quantidade = quantidade;
+    }
+
+    public Produto() {
+        super();
+    }
+}
